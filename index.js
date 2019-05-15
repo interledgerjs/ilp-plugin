6a29cc51f90ec165ab09e1019f1e6547dce4e7da
sentientwaffle <sentientwaffle@gmail.com>
2019-05-15 11:19:41 -0700

    feat: add ILP_BTP_SERVER env variable
    Allow callers to specify a server host without the whole URI. This makes sure a random secret is still generated for each plugin.
    
    When multiple plugins share a secret, they all get the same ilp address from mini-accounts, so all of them get passed each others' packets.

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ const crypto = require('crypto')
 
 function pluginFromEnvironment (opts) {
   const module = process.env.ILP_PLUGIN || 'ilp-plugin-btp'
-  const secret = require('crypto').randomBytes(16).toString('hex')
-  const name = (opts && opts.name) || ''
-  const credentials = process.env.ILP_CREDENTIALS
-    ? JSON.parse(process.env.ILP_CREDENTIALS)
-    : { server: `btp+ws://${name}:${secret}@localhost:7768` }
+  const credentials = generateCredentials(opts)
 
   log.debug('creating plugin with module', module)
   log.debug('creating plugin with credentials', credentials)
   const Plugin = require(module)
   return new Plugin(credentials)
+}
+
+function generateCredentials (opts) {
+  if (process.env.ILP_CREDENTIALS) {
+    return JSON.parse(process.env.ILP_CREDENTIALS)
+  }
+
+  const secret = require('crypto').randomBytes(16).toString('hex')
+  const name = (opts && opts.name) || ''
+  const server = process.env.ILP_BTP_SERVER || 'localhost:7768'
+
+  return { server: `btp+ws://${name}:${secret}@${server}` }
 }
 
 module.exports = pluginFromEnvironment
